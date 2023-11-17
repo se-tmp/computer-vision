eb86af0f2efdac81331a7254cfdf53692bc11d15
r4pidstart <r4pidstart@gmail.com>
2023-11-17 16:50:16 +0900

    model for demo

--- readme.md
+++ readme.md
@@ -10,7 +10,7 @@
 
 <!-- [OpenPose](https://github.com/CMU-Perceptual-Computing-Lab/openpose) -->
 
-## Datasets
+## Used datasets
 
 https://universe.roboflow.com/apollo-solutions-dev/sitting-and-standing
 
@@ -19,3 +19,7 @@ https://universe.roboflow.com/ctuet/fall_ip_camera
 https://universe.roboflow.com/itrvn/action-patient-recognition-version-fit-human/dataset/8
 
 https://universe.roboflow.com/lingfeng-gu/pose-usyfy/dataset/2
+
+https://universe.roboflow.com/tni-u40sv/activity-xqolm
+
+https://universe.roboflow.com/hanyang-university-derrc/activity-recognition-2vbeb
--- testing/test.py
+++ testing/test.py
@@ -3,53 +3,42 @@ import cv2
 import math 
 # start webcam
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+cap.set(3, 1920)
+cap.set(4, 1080)
 
-# model
-model = YOLO("best.pt")
-
-# object classes
-classNames = ["Lying", "Sitting", "Standing", "empty"]
-
+model = YOLO("pretrained_model.pt")
+classNames = ["Lying", "Sitting", "Standing", ""]
 
 while True:
     success, img = cap.read()
     results = model(img, stream=True)
 
-    # coordinates
     for r in results:
         boxes = r.boxes
 
         for box in boxes:
-            # bounding box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
             cls = int(box.cls[0])
             print("Class name -->", classNames[cls])
 
-            # put box in cam
             if(cls==0):
-                color = (0, 177, 177)
+                color = (0, 255, 255)
             elif(cls==1):
-                color = (177, 0, 177)
+                color = (255, 0, 255)
             else:
-                color = (177, 177, 0)
+                color = (255, 255, 0)
 
-            cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+            cv2.rectangle(img, (x1, y1), (x2, y2), color, 5)
 
-            # confidence
             confidence = math.ceil((box.conf[0]*100))/100
             print("Confidence --->",confidence)
 
-            # class name
-
-            # object details
             org = [x1, y1]
             font = cv2.FONT_HERSHEY_SIMPLEX
-            fontScale = 1
-            thickness = 2
+            fontScale = 5
+            thickness = 4
 
             cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
 
