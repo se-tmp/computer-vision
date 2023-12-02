bc94e9bd5e32ed717a3c43ec37c2c73a1530195f
r4pidstart <r4pidstart@gmail.com>
2023-12-02 15:20:16 +0900

    added fcmManager

---
+++ processing/FcmManager.py
@@ -0,0 +1,44 @@
+import argparse
+import json
+import requests
+import google.auth.transport.requests
+from google.oauth2 import service_account
+
+PROJECT_ID = 'se-tmp'
+BASE_URL = 'https://fcm.googleapis.com'
+FCM_ENDPOINT = 'v1/projects/' + PROJECT_ID + '/messages:send'
+FCM_URL = BASE_URL + '/' + FCM_ENDPOINT
+SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
+
+class FcmManager:
+    def _get_access_token(self):
+        credentials = service_account.Credentials.from_service_account_file(
+            'se-tmp-pk.json', scopes=SCOPES)
+        request = google.auth.transport.requests.Request()
+        credentials.refresh(request)
+        return credentials.token
+
+    def send_fcm_message(self, routineNumber):
+        headers = {
+            'Authorization': 'Bearer ' + self._get_access_token(),
+            'Content-Type': 'application/json; UTF-8',
+        }
+        resp = requests.post(FCM_URL, data=json.dumps(self._build_message(routineNumber)), headers=headers)
+
+        if resp.status_code == 200:
+            print('Message sent to Firebase for delivery, response:')
+            print(resp.text)
+        else:
+            print('Unable to send message to Firebase')
+            print(resp.text)
+
+    def _build_message(self, routineNumber):
+        return {
+            'message': {
+                'topic': 'all',
+                'data': {
+                    'title': 'start routine',
+                    'body': str(routineNumber)
+                }
+            }
+        }
