b30eb4d04ceed0a89146c625648a4491d5796c06
r4pidstart <r4pidstart@gmail.com>
2023-12-03 09:34:54 +0900

    If posture does't change, trigger won't be activated

--- processing/Processor.py
+++ processing/Processor.py
@@ -5,7 +5,7 @@ from FcmManager import FcmManager
 from Firestore import Firestore
 import time
 
-classNames = ["Lying", "Sitting", "Standing", ""]
+classNames = ["lie", "sit", "stand", ""]
 # box = {{x1, y1}, {x2, y2}, cls}
 
 class Processor(object):
@@ -15,6 +15,7 @@ class Processor(object):
         self.prev = 3
         self.fcmManager = FcmManager()
         self.firestore = Firestore()
+        self.prev = set()
         
         self.thread = Thread(target=self.process, args=())
         self.thread.daemon = True
@@ -25,32 +26,38 @@ class Processor(object):
         # get the euclidean dist between lt points
         return math.sqrt((box1[0][0] - box2[0][0])**2 + (box1[0][1] - box2[0][1])**2)
     
-    def _group_similar_points(self, points, threshold = 0.07):
+    def _group_similar_points(self, boxes, threshold = 0.07):
         groups = []
         
-        for point in points:
+        for box in boxes:
             added_to_group = False
             for group in groups:
-                if any(self._get_dist(point, group_point) <= threshold for group_point in group):
-                    group.append(point)
+                group_type = group[0][2]
+                if box[2] == group_type and any(self._get_dist(box, group_point) <= threshold for group_point in group):
+                    group.append(box)
                     added_to_group = True
                     break
         
             if not added_to_group:
-                groups.append([point])
+                groups.append([box])
         return groups
     
     def _get_center(self, point1, point2):
-        return ((point1[0]+point2[0])/2, (point1[1]+point2[1])/2)
+        return ((point1[0]+point2[0])/2, (point1[1]+point2[1])/2, point1[2])
 
     def _check_trigger(self, point):
         ret = []
+        res = set()
         triggers = self.firestore.db.collection(u'trigger').get()
 
         for trigger_raw in triggers:
             trigger = trigger_raw.to_dict()
-            if trigger['lt_x'] <= point[0] <= trigger['rb_x'] and trigger['lt_y'] <= point[1] <= trigger['rb_y']:
-                ret.append(trigger['routine_id'])
+            if trigger['detect_posture'] == point[2] and trigger['lt_x'] <= point[0] <= trigger['rb_x'] and trigger['lt_y'] <= point[1] <= trigger['rb_y']:
+                if not (trigger['routine_id'] in self.prev):
+                    ret.append(trigger['routine_id'])
+                set.add(trigger['routine_id'])
+        
+        self.prev = res
 
         return ret        
 
